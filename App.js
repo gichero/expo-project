244c55d8a4ea87d78f0564860562642700873d7a
Amos <amosgathungu@gmail.com>
2017-10-08 21:13:27 -0400

    root file

--- App.js
+++ App.js
@@ -1,50 +1,15 @@
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-//
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Amos Gichero's App</Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+//import library to help create component
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import Login from './src/Components/Login/Login.js';
 
-//import libraries to create component
-import React from 'react';
-import { Text, View } from 'react-native';
-
-//make a component
-const Header = () => {
-    const { textStyle, viewStyle } = styles;
-
-    return(
-
-    <View style = {viewStyle}>
-        <Text style = { textStyle }>Lost and Found</Text>
-    </View>
-
-    );
-};
-
-const styles = {
-    viewStyle: {
-        backgroundColor: '#e6f2ff'
-    },
-    textStyle: {
-        fontSize: 25
+//Create component
+export default class Lost_found extends Component{
+    render(){
+        return(
+            <Login />
+        );
     }
-};
+}
 
-//make the component available to other parts of the app
-export default Header;
+AppRegistry.registerComponent('lost_found', () => Lost_found);
