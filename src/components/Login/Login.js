c1a99ccb2ad01a33300c9130ca013617ac6dc7f2
Amos <amosgathungu@gmail.com>
2017-10-09 12:27:33 -0400

    image added and styling

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,28 @@
 //import libraries to create component
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, KeyboardAvoidingView } from 'react-native';
+import LoginForm from './LoginForm.js';
 
 //make a component
 export default class Login extends Component {
     render(){
         return (
-            <View style = {styles.container}>
+            <KeyboardAvoidingView behavior = 'padding' style = {styles.container}>
+
                 <View style = {styles.logoContainer}>
-
+                    <image style = {styles.logo}
+                    source={require('../../images/lostfound.png')}
+                    />
+                    <Text style = {styles.title}>
+                    An app that helps you find your lost items
+                    </Text>
                 </View>
+
                 <View style = {styles.formContainer}>
-
+                    <LoginForm />
                 </View>
 
-            </View>
+            </KeyboardAvoidingView>
         );
     }
 }
@@ -22,5 +30,21 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#3498db'
+    },
+    logoContainer: {
+        alignItems: 'center',
+        flexGrow: 1,
+        justifyContent: 'center'
+    },
+    logo: {
+        width: 100,
+        height: 100
+    },
+    title: {
+        color: '#FFFFFF',
+        marginTop: 10,
+        width: 160,
+        textAlign: 'center',
+        opacity: 0.9
     }
 });
